f6a6b3019c659148d390a24d2a09e6cf00bf549f
mac2 <mauricio.trevinon91@gmail.com>
2023-05-17 19:42:35 -0600

    Define Category, Product and tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', // In case the associated category is deleted, the product will also be deleted
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', 
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_products',
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE', 
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE', 
+});
 
 module.exports = {
   Product,
