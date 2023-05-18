4ac19809b3060602c6b9f6aec75865e6e745e63f
mac2 <mauricio.trevinon91@gmail.com>
2023-05-18 00:36:09 -0600

    Changed port, now its working 🥲

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
-const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+
+// it can also work with port 3001
+const PORT = process.env.PORT || 3002;
+const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+async function startServer() {
+  try {
+    await sequelize.sync({ force: false });
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.error('Unable to sync database:', err);
+  }
+}
+
+startServer();
